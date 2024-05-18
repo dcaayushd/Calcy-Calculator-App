91c4ac8c651fbc6f00378e5ac6dac4709015cb85
Aayush <aayushdc123@yahoo.com>
2024-05-18 08:13:08 +0545

    Controllers Binded

---
+++ lib/bindings/my_bindings.dart
@@ -0,0 +1,11 @@
+import 'package:calcy_calculatorapp/controller/calculate_controller.dart';
+import 'package:calcy_calculatorapp/controller/theme_controller.dart';
+import 'package:get/instance_manager.dart';
+
+class MyBindings implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut(() => CalculateController());
+    Get.lazyPut(() => ThemeController());
+  }
+}
