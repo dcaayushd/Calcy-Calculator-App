e3e2bdb714a7132f1172e9d3b728772425a29798
Aayush <aayushdc123@yahoo.com>
2024-05-18 08:14:02 +0545

    Colors Added

---
+++ lib/utils/colors.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+/// Dark Theme Colors
+class DarkColors {
+  static const scaffoldBgColor = Color(0xff22252D);
+  static const sheetBgColor = Color(0xff292D36);
+  static const btnBgColor = Color.fromARGB(255, 33, 36, 42);
+  static const leftOperatorColor = Color.fromARGB(255, 7, 255, 209);
+}
+
+/// Light Them Colors
+class LightColors {
+  static const scaffoldBgColor = Color(0xffFFFFFF);
+  static const sheetBgColor = Color(0xffF9F9F9);
+  static const btnBgColor = Color.fromARGB(255, 243, 243, 243);
+  static const operatorColor = Colors.deepPurpleAccent;
+  static const leftOperatorColor = Color.fromARGB(255, 1, 157, 128);
+}
